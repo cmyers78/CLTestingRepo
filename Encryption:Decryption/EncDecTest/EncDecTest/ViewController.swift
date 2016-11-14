8166566f3ee0747ea6bf917363d543c9f7c8ec6f
Chris Myers <cmyers78@gmail.com>
2016-11-14 13:31:23 -0500

    minor changes....testing array to string methods, etc...

--- Encryption:Decryption/EncDecTest/EncDecTest/ViewController.swift
+++ Encryption:Decryption/EncDecTest/EncDecTest/ViewController.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let getLit = ([UInt8](), Int())
+    
+    let getLitArray = [UInt8]()
+    let getLitInt = Int()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // create instance in View Did Load
         let keyFunction = SecureData()
-        
         keyFunction.createKeyPair()
+        
+        // Encrypt after ????
         let answer = keyFunction.encryptData(stringToEncrypt: "Get Lit Whit...")
+        
+        // Decrypt before sending to server
         keyFunction.decryptData(messageEncrypted: answer.0, messageEncryptedSize: answer.1)
+        
+        let array = ["1", "2", "3"]
+        
+        let arrayToString = array.joined(separator: ",")
+        
+        print("My array is : \(arrayToString)")
         
     }
 
