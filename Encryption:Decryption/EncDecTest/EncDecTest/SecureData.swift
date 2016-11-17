81ff6c4c2ceb9a0f366cf25394bc91be101e66b0
Chris Myers <cmyers78@gmail.com>
2016-11-17 14:38:28 -0500

    code cleanup, added MARKS notes

--- Encryption:Decryption/EncDecTest/EncDecTest/SecureData.swift
+++ Encryption:Decryption/EncDecTest/EncDecTest/SecureData.swift
@@ -8,29 +8,23 @@
 
 import Foundation
 
-
 class SecureData : NSObject {
-    
+
+    // MARK : Class properties
     var publicKey : SecKey?
     var privateKey : SecKey?
-    
     var encToDec : [UInt8]?
     var encToDecSize : Int?
-    
-    
+
     override init() {
         super.init()
-        
     }
     
     func createKeyPair() {
-        
         var statusCode : OSStatus
 
-        
         let publicKeyAttr : [NSString : AnyObject] = [kSecAttrIsPermanent: true as AnyObject, kSecAttrApplicationTag:"publicTag".data(using: .utf8)! as AnyObject]
         let privateKeyAttr : [NSString : AnyObject] = [kSecAttrIsPermanent: true as AnyObject, kSecAttrApplicationTag:"privateTag".data(using: .utf8)! as AnyObject]
-        
         
         var keyPairAttr = [NSObject : NSObject]()
         keyPairAttr[kSecAttrKeyType] = kSecAttrKeyTypeRSA
@@ -51,53 +45,37 @@ class SecureData : NSObject {
             print("Error generating key pair: \(statusCode)")
         }
     }
-    
+    // MARK : Encrypt Data function using public key
     func encryptData(stringToEncrypt : String) -> ([UInt8], Int)  {
         
-        
-        //var statusCode : OSStatus
-        
         let stringEnc = stringToEncrypt
-        
         let stringEncData = [UInt8](stringEnc.utf8)
-        
         let stringEncDataLength = UInt(stringEnc.characters.count)
-        
         let blockSize = SecKeyGetBlockSize(self.publicKey!)
         
         var messageEncrypted = [UInt8](repeating: 0, count: blockSize)
-        
         var messageEncryptedSize = blockSize
         
         let result = SecKeyEncrypt(publicKey!, SecPadding.PKCS1, stringEncData, Int(stringEncDataLength), &messageEncrypted, &messageEncryptedSize)
         
         if result != noErr {
             print("Encryption Error")
-        
         } else {
             print("here comes the encrypted result...")
-            
             print(messageEncrypted)
             self.encToDec = messageEncrypted
             self.encToDecSize = messageEncryptedSize
             print(result)
         }
-        
         return (messageEncrypted, messageEncryptedSize)
-        
     }
-    
+    // MARK : Decrypt encrypted data message using private key
     func decryptData(messageEncrypted : [UInt8], messageEncryptedSize : Int) {
         
         let intDec = messageEncrypted
         let intDecSize = messageEncryptedSize
         
-        
-        
-        // turn int To Decrypt into data??
-        
         var messageDecryptedBlockSize = SecKeyGetBlockSize(self.privateKey!)
-        
         var messageDecrypted = [UInt8](repeating: 0, count: messageDecryptedBlockSize)
         
         let result = SecKeyDecrypt(self.privateKey!, SecPadding.PKCS1, intDec, intDecSize, &messageDecrypted, &messageDecryptedBlockSize)
@@ -110,12 +88,6 @@ class SecureData : NSObject {
             let decryptedText = String(bytes: messageDecrypted, encoding: .utf8)
             print("Here comes the decrypted text: ")
             print(decryptedText!)
-            
         }
-        
-        
-        
-        
     }
-    
 }
--- Encryption:Decryption/EncDecTest/EncDecTest/ViewController.swift
+++ Encryption:Decryption/EncDecTest/EncDecTest/ViewController.swift
@@ -9,28 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        // create instance in View Did Load
-        let keyFunction = SecureData()
-        keyFunction.createKeyPair()
+        // instance of SecureData Class
+        let kingInTheNorth = SecureData()
         
-        // Encrypt after ????
-        let answer = keyFunction.encryptData(stringToEncrypt: "Winter is coming.")
-        
-        // Decrypt before sending to server
-        keyFunction.decryptData(messageEncrypted: answer.0, messageEncryptedSize: answer.1)
-        
-        
+        kingInTheNorth.createKeyPair()
+        let answer = kingInTheNorth.encryptData(stringToEncrypt: "Winter is coming...")
+        kingInTheNorth.decryptData(messageEncrypted: answer.0, messageEncryptedSize: answer.1)
     }
-
-    
-
-
 }
 
