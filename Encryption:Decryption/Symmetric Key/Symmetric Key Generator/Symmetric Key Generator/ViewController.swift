2ef2d05b86ef9f1e889830bea458cd4cb3476fdf
Chris Myers <cmyers78@gmail.com>
2016-11-23 09:48:45 -0500

    working well.  need to check on creating random key and initialization vector

--- Encryption:Decryption/Symmetric Key/Symmetric Key Generator/Symmetric Key Generator/ViewController.swift
+++ Encryption:Decryption/Symmetric Key/Symmetric Key Generator/Symmetric Key Generator/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Iubet igitur nos Pythius Apollo noscere nosmet ipsos. Nihilo beatiorem esse Metellum quam Regulum. Sed tamen enitar et, si minus multa mihi occurrent, non fugiam ista popularia. Tecum optime, deinde etiam cum mediocri amico. Restinguet citius, si ardentem acceperit. Quis, quaeso, illum negat et bonum virum et comem et humanum fuisse? Itaque nostrum est-quod nostrum dico, artis est-ad ea principia, quae accepimus. Duo Reges: constructio interrete. Restinguet citius, si ardentem acceperit. Nam de isto magna dissensio est. Mihi, inquam, qui te id ipsum rogavi? Quis Pullum Numitorium Fregellanum, proditorem, quamquam rei publicae nostrae profuit, non odit? Quicquid enim a sapientia proficiscitur, id continuo debet expletum esse omnibus suis partibus; Hic quoque suus est de summoque bono dissentiens dici vere Peripateticus non potest. Dic in quovis conventu te omnia facere, ne doleas. Universa enim illorum ratione cum tota vestra confligendum puto. Est enim effectrix multarum et magnarum voluptatum. Cupiditates non Epicuri divisione finiebat, sed sua satietate. Quis negat? Illum mallem levares, quo optimum atque humanissimum virum, Cn. Vestri haec verecundius, illi fortasse constantius. Quid nunc honeste dicit? Reguli reiciendam; Quid in isto egregio tuo officio et tanta fide-sic enim existimo-ad corpus refers? Quae diligentissime contra Aristonem dicuntur a Chryippo. Primum in nostrane potestate est, quid meminerimus? Inscite autem medicinae et gubernationis ultimum cum ultimo sapientiae comparatur. Tum ille timide vel potius verecunde: Facio, inquit. Tecum optime, deinde etiam cum mediocri amico. Quia dolori non voluptas contraria est, sed doloris privatio. Aliena dixit in physicis nec ea ipsa, quae tibi probarentur; Sed quid minus probandum quam esse aliquem beatum nec satis beatum? Sed ad illum redeo. Non quam nostram quidem, inquit Pomponius iocans; Si de re disceptari oportet, nulla mihi tecum, Cato, potest esse dissensio. Quamquam id quidem licebit iis existimare, qui legerint. Sed quid attinet de rebus tam apertis plura requirere? Quid nunc honeste dicit? Quam illa ardentis amores excitaret sui! Cur tandem? Sit enim idem caecus, debilis."
+        let text = "The forebearing use of power does not only form a touchstone but the manner in which a gentleman has advantages over others is the test of a true gentleman."
         
         do {
             let aes = try AES(key: "passwordpassword", iv: "drowssapdrowssap")
@@ -25,9 +25,12 @@ class ViewController: UIViewController {
             print(ciperText)
             print(decrypted)
             let result = String(bytes: decrypted, encoding: .utf8)
-            print(result)
+            print(result ?? String())
             
-        } catch { }
+        } catch {
+            print("encryption with CryptoSwift failed")
+            
+        }
         
         
         // Do any additional setup after loading the view, typically from a nib.
